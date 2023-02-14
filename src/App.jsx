7ac6c71b9034569e841f4499a5e537b5bdecc808
Juan Cantor <sebastiancantorvc@gmail.com>
2023-02-13 21:56:45 -0500

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,44 +3,79 @@ import './App.css'
 import { Auth } from './components/auth'
 import { db } from '../config/firebase'
 import { useEffect, useState } from 'react'
-import { getDocs, collection } from 'firebase/firestore'
+import { getDocs, collection, addDoc, deleteDoc, doc } from 'firebase/firestore'
+import { async } from '@firebase/util'
+
+
+
 function App() {
 
   const [MovieList, setMovieList] = useState([])
   const moviesColletionRef = collection(db, "movies")
 
-  useEffect(() => {
-    const getMovieList = async () => {
-      try {
-        const data = await getDocs(moviesColletionRef);
-        const filteredData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
-        setMovieList(filteredData)
-      } catch (err) {
-        console.log(err);
-      }
-    }
 
+  //NEW MOVIE STATES
+
+  const [newMovieTitle, setnewMovieTitle] = useState("")
+  const [newReleaseDate, setnewReleaseDate] = useState("")
+  const [IsNewMovieWinner, setIsNewMovieWinner] = useState(false)
+
+
+
+  const getMovieList = async () => {
+    try {
+      const data = await getDocs(moviesColletionRef);
+      const filteredData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+      setMovieList(filteredData)
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  useEffect(() => {
     getMovieList();
   }, [])
+
+
+  const onSubmitMovie = async () => {
+    try {
+      await addDoc(moviesColletionRef, { title: newMovieTitle, realeaseDate: newReleaseDate, Nominated: IsNewMovieWinner })
+      getMovieList();
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
+
+
+  const deleteMovie = async (id) => {
+    const movieDoc = doc(db, "movies", id)
+    await deleteDoc(movieDoc)
+    getMovieList();
+  }
 
   return (
     <>
       <Auth />
 
       <div>
-        <input type="text" placeholder='Movie Title...' />
-        <input type="number" placeholder='Release Date...' />
-        <input type="checkbox" placeholder='Release Date...' />
+        <input type="text" placeholder='Movie Title...'
+          onChange={(e) => setnewMovieTitle(e.target.value)} />
+        <input type="number" placeholder='Release Date...'
+          onChange={(e) => setnewReleaseDate(Number(e.target.value))} />
+        <input type="checkbox" placeholder='Release Date...'
+          checked={IsNewMovieWinner} onChange={(e) => setIsNewMovieWinner(e.target.checked)} />
         <label htmlFor="">Premiadas</label>
-        <button>Submit Movie</button>
+        <button onClick={onSubmitMovie}>Submit Movie</button>
       </div>
 
 
       <div>
         {MovieList.map((movie) => (
-          <div>
+          <div key={movie.id}>
             <h1>{movie.title}</h1>
             <p>Year of release:{movie.realeaseDate}</p>
+            <button onClick={() => deleteMovie(movie.id)}>Delete Movie</button>
           </div>
         ))}
       </div>
